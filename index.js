ce31768a9139947a127142e14cb325ec636f4d14
John Waithaka <johnwaruiwaithaka@gmail.com>
2024-04-18 11:32:31 +0300

    success but main error

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
 
 // Write your code here
+
+
+//instructions
+
+// Create a variable called multiply set to an equation that will multiply the variables num1 and num2; the result of the multiplication should be 62.
+// Create a variable called random that will generate a random integer greater than 0.
+// Create a variable called mod set to an equation that will calculate the remainder of dividing variable num3 by num4; the remainder should be 4.
+// Create a variable called max that finds the highest number in a set; the value returned should be 20.
+
+
+//review the lab
+//test 1
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
+
+//test 2
+const random = Math.floor( Math.random() * 1000 ) + 1;
+console.log(random);
+
+//test 3
+const num3 = 20;
+const num4 = 5;
+const mod = num3 % num4;
+
+//test 4
+const max = Math.max( 4, 8, 12, 16, 20 );
